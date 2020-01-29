86f50480910bab60b12ce3f6d3c6c40cbe0bac9d
zsumon <sumon143014@gmail.com>
2020-01-30 00:15:29 +0600

    some background images added

--- app/src/main/java/com/app/bookassistant/MainActivity.kt
+++ app/src/main/java/com/app/bookassistant/MainActivity.kt
@@ -4,19 +4,40 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.app.bookassistant.data.models.Book
+import com.app.bookassistant.ui.dashboard.BookListAdapter
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var bookListAdapter: BookListAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        hello_id?.text = "This is demo text view item"
-
+        initDashboardBookList()
     }
 
-    fun onButtonClick(view: View) {
-        Toast.makeText(this, "Clicked", Toast.LENGTH_SHORT).show()
+    fun initDashboardBookList() {
+        dashboardBookListRecyclerView.apply {
+            layoutManager = LinearLayoutManager(this@MainActivity)
+            bookListAdapter = BookListAdapter()
+            adapter = bookListAdapter
+        }
+        val items = mutableListOf<Book>()
+        for (i in 0..5) {
+            val b = Book()
+            b.apply {
+                title = "Book $i"
+                description = "Description of Book $i"
+            }
+            items.add(b)
+        }
+        bookListAdapter.supplyBookList(items)
+
+        //
+        /// bookListAdapter.notifyDataSetChanged()
     }
 }
--- app/src/main/java/com/app/bookassistant/ui/dashboard/BookListAdapter.kt
+++ app/src/main/java/com/app/bookassistant/ui/dashboard/BookListAdapter.kt
@@ -1,28 +1,75 @@
 package com.app.bookassistant.ui.dashboard
 
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.app.bookassistant.R
 import com.app.bookassistant.data.models.Book
+import kotlinx.android.synthetic.main.dashboard_books.view.*
+
 
 class BookListAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
 
     private var bookList: List<Book> = mutableListOf()
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
-        //        TODO
+        return BooklistViewHolder(
+            LayoutInflater.from(parent.context).inflate(
+                R.layout.dashboard_books, parent, false
+            )
+        )
+    }
+
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        when (holder) {
+            is BooklistViewHolder -> {
+                holder.bind(bookList[position])
+            }
+        }
     }
 
     override fun getItemCount(): Int {
         return bookList.size
     }
 
-    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
-
+    fun supplyBookList(bookList: List<Book>) {
+        this.bookList = bookList
     }
 
+
     class BooklistViewHolder constructor(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        fun bind(book: Any) {}
+        private val IMGS = mutableListOf(
+            R.drawable.img_badminton,
+            R.drawable.img_baseball,
+            R.drawable.img_bowling,
+            R.drawable.img_cycling,
+            R.drawable.img_golf,
+            R.drawable.img_running,
+            R.drawable.img_soccer,
+            R.drawable.img_basketball,
+            R.drawable.img_swimming,
+            R.drawable.img_tabletennis,
+            R.drawable.img_tennis
+        )
+
+        private val bookTitle = itemView.book_title
+        private val bookDescriptor = itemView.book_description
+        private val moreBtn = itemView.more_button
+        private val bookItemLayout = itemView.bookItemLayout
+
+        fun bind(book: Book) {
+            bookTitle.text = book.title
+            bookDescriptor.text = book.description
+
+            setBackground()
+        }
+
+        private fun setBackground() {
+            val ran = (0 until IMGS.size).random()
+            bookItemLayout.setBackgroundResource(IMGS[ran])
+        }
     }
 
 }
