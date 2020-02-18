3ebc475b0af3be89cf0ee4380e5ad808b2ab6859
zsumon <sumon143014@gmail.com>
2020-02-19 00:24:03 +0600

    confirmation before adding to enrolled done

--- app/src/main/java/com/app/bookassistant/ui/dashboard/AvailableBookAdapter.kt
+++ app/src/main/java/com/app/bookassistant/ui/dashboard/AvailableBookAdapter.kt
@@ -1,14 +1,21 @@
 package com.app.bookassistant.ui.dashboard
 
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
+import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.Window
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.DialogFragment
 import androidx.recyclerview.widget.RecyclerView
 import com.app.bookassistant.R
+import kotlinx.android.synthetic.main.dialog_confirm_enroll_book.*
 import kotlinx.android.synthetic.main.item_available_book.view.*
 
 class AvailableBookAdapter(private val onAvailableBookListener: OnAvailableBookListener) :
-    RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+    RecyclerView.Adapter<RecyclerView.ViewHolder>(), ConfirmEnrollBookDialog.OnDialogListener {
 
     lateinit var availableBook: MutableList<BookModel>
 
@@ -25,16 +32,17 @@ class AvailableBookAdapter(private val onAvailableBookListener: OnAvailableBookL
     }
 
     override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
-
         when (holder) {
             is AvailableViewHolder -> {
                 holder.bind(availableBook[position])
                 holder.itemView.setOnClickListener {
-                    onAvailableBookListener.onAvailableBookClick(position)
+                    ConfirmEnrollBookDialog(this, position).show(
+                        (onAvailableBookListener as AppCompatActivity).supportFragmentManager,
+                        "show_available_book_to_enroll_confirm_dialog"
+                    )
                 }
             }
         }
-
     }
 
     fun supplyList(items: MutableList<BookModel>) {
@@ -58,4 +66,47 @@ class AvailableBookAdapter(private val onAvailableBookListener: OnAvailableBookL
         fun onAvailableBookClick(position: Int)
     }
 
+    override fun onConfirmClick(position: Int) {
+        onAvailableBookListener.onAvailableBookClick(position)
+    }
+
+    override fun onCancelClick(position: Int) {
+
+    }
+
+}
+
+public class ConfirmEnrollBookDialog(
+    private val onDialogListener: OnDialogListener,
+    private val position: Int
+) : DialogFragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.dialog_confirm_enroll_book, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        dialog?.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+        dialog?.window?.requestFeature(Window.FEATURE_NO_TITLE)
+
+        conf_dialog_confirm.setOnClickListener {
+            onDialogListener.onConfirmClick(position)
+            dialog?.dismiss()
+        }
+
+        conf_dialog_cancel.setOnClickListener {
+            // onDialogListener.onCancelClick(position)
+            dialog?.dismiss()
+        }
+    }
+
+    interface OnDialogListener {
+        fun onConfirmClick(position: Int)
+        fun onCancelClick(position: Int)
+    }
 }
